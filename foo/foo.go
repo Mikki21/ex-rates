dc8d24bba99de115ec6c0d9d9fdc097601dfd491
Misha Pacheskiy <mikki.s.fails@gmail.com>
2018-11-22 17:24:52 +0200

    123

--- foo/foo.go
+++ foo/foo.go
@@ -3,25 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"os"
-	"strconv"
 
-	"github.com/astaxie/beego"
-	_ "github.com/oreuta/easytrip/routers"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	var err error
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	beego.BConfig.Listen.HTTPPort, err = strconv.Atoi(port)
-	if err != nil {
-		panic(err)
-	}
 
 	db, err := sql.Open("mysql", "b44aea942d06ef:ca7587cd@us-cdbr-iron-east-01.cleardb.net/heroku_c1b06006d977072")
 
@@ -37,5 +23,4 @@ func main() {
 	}
 	defer insert.Close()
 
-	beego.Run()
 }
